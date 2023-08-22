b676d56db25858ccd12a0a79e8dc127f4a77bb12
sidney <516202795@qq.com>
2023-08-22 23:30:14 +0800

    init todo

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+use std::collections::HashMap;
+
+#[derive(Debug)]
+struct Todo {
+    id: usize,
+    content: String,
+    done: bool,
+}
+
+fn main() {
+    println!("Hello, world!");
+    let mut todos = HashMap::new();
+    todos.insert(1, Todo {
+        id: 1,
+        content: "学英语".to_string(),
+        done: false
+     });
+     let mut next_id = 2;
+     loop {
+         let mut input = String::new();
+         std::io::stdin().read_line(&mut input).unwrap();
+
+         let cmd = input.trim();
+         let mut parts = cmd.split_whitespace();
+         if let Some(cmd) = parts.next() {
+            match cmd {
+                "LIST" => {
+                    println!("{:?}", todos);
+                }
+                "ADD" => {
+                    if let Some(add_content) = parts.next() {
+                        todos.insert(next_id, Todo { id: next_id, content: add_content.to_string(), done: false });
+                        next_id += 1;
+                    }
+                }
+                _ => {}
+            }
+         }
+
+     }
+}
